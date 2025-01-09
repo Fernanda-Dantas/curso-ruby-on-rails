f66105e6056a84991896d136166a17e650d241f4
Fernanda <fer.dantas07@gmail.com>
2025-01-09 14:48:16 -0300

    instalei o sass

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,6 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( coins.css mining_types.css scaffolds.css welcome.css )
+
+Rails.application.config.assets.precompile += %w( coins.js welcome.js mining_types.js )
